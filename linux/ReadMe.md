533ad3f76ea5227bf1c3c8441e6002ffa04c3f42
sandiprb <baradiya.sandip@gmail.com>
2020-03-23 11:47:33 +0530

    organised files and bynch of new scripts added

--- linux/ReadMe.md
+++ linux/ReadMe.md
@@ -1,4 +1,5 @@
 # Linux System Utils
 
 - [How To Set or Change Timezone on Ubuntu](https://github.com/sandiprb/stuff/blob/master/linux/how-to-set-timezone-on-ubuntu.md)
+- [Deploying Django App on Ubuntu](https://github.com/sandiprb/stuff/blob/master/linux/ServerSetup-Makefile)
 
---
+++ python_script/domain_mx_check.py
@@ -0,0 +1,15 @@
+import logging
+
+# Note: dnspython dependency is required for this to work
+import dns.resolver, dns.exception
+
+logger = logging.getLogger(__name__)
+
+def is_valid_domain(domain):
+    try:
+        logger.debug('Checking domain %s', domain)
+        dns.resolver.query(domain, 'MX')
+        return True
+    except dns.exception.DNSException as e:
+        logger.debug('Domain %s does not exist.', e)
+        return False
